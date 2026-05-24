884b48a08213a44cd830dfb84f62d7bd217d6007
SunsetLoom7594 <marshallsamuel130@gmail.com>
2026-05-24 08:17:24 +0100

    Update version number to 1.0.05

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 return {
     name = "Creepux OS - Server Edition",
     info = "NO-EDIT","BETA-VERSION",
-    version = "1.0.04",
+    version = "1.0.05",
     platform = "Linux",
     extrasinstalled = false
 }
